5c4aa039a30d2b1b9dee497194ab630a88097401
Shivam Budhia <shivam.budhia27@gmail.com>
2018-10-19 00:03:30 +0530

    Add files via upload

---
+++ _credentials.py
@@ -0,0 +1,6 @@
+# Fill here your own Twitter Application Credentials (see https://apps.twitter.com/)
+
+ACCESS_TOKEN =      '3016310796-WVoxFaq7d7ZOo6k8KTiB3RUpPVKWB2Fts2939PX'
+ACCESS_SECRET =     'TM2AUbQC8z9PNSo0psS7CaZejnHdbBnUDBTLev7fPSf5D'
+CONSUMER_KEY =      'FxD6NpXKwRAPl7Qe5hfSOB2Ss'
+CONSUMER_SECRET =   'ke8UUiU3lFbwCRqHzezBmIqEQHT3VNeice0ReeNf6z4miIZ1tx'
